2a9d31a64b6e8ba7630b98040afe2edf09d6dea9
Adham <adhamahmed1234321@gmail.com>
2022-05-17 12:40:22 +0200

    lab 1 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
-import Slider from './components/Slider/Slider'
-
+import { Slider } from './components/Slider/Slider';
+import Task1 from './components/task1';
 function App() {
   return (
     <div className="App">
+      <Task1></Task1>
       <Slider></Slider>
     </div>
   );
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,37 +1,57 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import {Component} from "react";
+export class Slider extends Component {
 
+    constructor(props) {
+      // console.log(props)
+        super(props);
+        this.state = {
+            'images': [
+              "logo192.png",
+              "img1.webp",
+              "sun-rise.jpg",
+              "pic.jpg",
+              "pic2.jpg",
+            ],
+            'index': 0,
+            'selectedImage': "img1.webp",
+            'intervalId': 0
+        }
+    };
+    prev= () => {
+        this.setState.index = this.state.index > 0 ? --this.getState.index : this.state.images.length - 1;
+        this.setState({
+          selectedImage: this.state.images[this.state.index]
+        });
+    };
 
+    render() {
 
+        return (
+            <div>
+                <img width="400px" height="400px" altt="slide show image" id="slideShow" alt="img"
+                     src={this.state.selectedImage}/>
+                <div>
+                    <input type={"button"} value="Prev" onClick={(event) => { this.prev() }}/> 
+                    <input type={"button"} value="Next" onClick={(event) => {
+                        this.setState.index = this.state.index < this.state.images.length - 1 ? ++this.getState.index : 0;
+                        this.setState({
+                            selectedImage: this.state.images[this.state.index]
+                        });
+                    }}/>
+                    <input type={"button"} value="Play" onClick={(event) => {
+                        let id = setInterval(() => {
+                            this.setState.index = this.state.index > 0 ? --this.getState.index : this.state.images.length - 1;
+                            this.setState({selectedImage: this.state.images[this.state.index]})
+                            this.setState({intervalId: id});
+                        }, 1000);
+                    }}/>
+                    <input type={"button"} value="Stop" onClick={(event) => {
+                        console.log(this.state.intervalId);
+                        clearInterval(this.state.intervalId);
+                    }}/>
+                </div>
 
-const Slider = () => (
-  
-  const pics=[
-    "logo192.png",
-    // "img1.webp",
-    // "sun-rise.jpg",
-    // "pic.jpg",
-    // "pic2.jpg",
-  ],
- <div className="slider">
-    <div>
-   {{
-        // var pics[];
-
-   }}
-    <img src={pics[0]} alt="placeholder" style={{width:"50%"}}/>
-  </div>
-  
-  <div>
-  <button className="btn btn-primary" > left</button>
-  <span> </span>
-  <button className="btn btn-primary"> right</button>
-  </div>
- </div>
-);
-
-Slider.propTypes = {};
-
-Slider.defaultProps = {};
-
-export default Slider;
+            </div>
+        )
+    }
+}
